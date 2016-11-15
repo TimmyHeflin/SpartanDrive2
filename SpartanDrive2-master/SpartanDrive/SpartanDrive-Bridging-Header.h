9b5001736c1a9899c70b5e11417d03021d5bc43f
duy nguyen <duynguyen@duys-MBP.hsd1.ca.comcast.net>
2016-11-14 19:46:17 -0800

    redirect to table view after successfully sign in

---
+++ SpartanDrive2-master/SpartanDrive/SpartanDrive-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SpartanDrive-Bridging-Header.h
+//  SpartanDrive
+//
+//  Created by Student on 11/5/16.
+//  Copyright © 2016 CMPE137. All rights reserved.
+//
+
+#import <GoogleSignIn/GoogleSignIn.h>
+#ifndef SpartanDrive_Bridging_Header_h
+#define SpartanDrive_Bridging_Header_h
+
+
+#endif /* SpartanDrive_Bridging_Header_h */
